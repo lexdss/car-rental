6ad6585afe0da07ed2da41b0340d72221f70dc61
Anon <lexdss@gmail.com>
2017-03-23 19:45:51 +0200

    Add Category model

---
+++ models/Category.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $slug
+ * @property string $short_description
+ * @property string $description
+ * @property string $img
+ * @property integer $up_date
+ *
+ * @property Car[] $cars
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'slug', 'up_date'], 'required'],
+            [['short_description', 'description'], 'string'],
+            [['up_date'], 'integer'],
+            [['name', 'slug', 'img'], 'string', 'max' => 255],
+            [['slug'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'slug' => 'Slug',
+            'short_description' => 'Short Description',
+            'description' => 'Description',
+            'img' => 'Img',
+            'up_date' => 'Up Date',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCars()
+    {
+        return $this->hasMany(Car::className(), ['category_id' => 'id']);
+    }
+}
