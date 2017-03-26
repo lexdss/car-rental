afea6c5512cd5583c379e066bd47833f43041df2
Anon <lexdss@gmail.com>
2017-03-26 22:59:59 +0300

    Add category CRUD

---
+++ models/admin/search/CategorySearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace app\models\admin\search;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Category;
+
+/**
+ * CategorySearch represents the model behind the search form about `app\models\Category`.
+ */
+class CategorySearch extends Category
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'up_date'], 'integer'],
+            [['name', 'slug', 'short_description', 'description', 'img'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Category::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'up_date' => $this->up_date,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'slug', $this->slug])
+            ->andFilterWhere(['like', 'short_description', $this->short_description])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'img', $this->img]);
+
+        return $dataProvider;
+    }
+}
