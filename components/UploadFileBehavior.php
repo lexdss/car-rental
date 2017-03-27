bc91d0db7dc327282b92dbff1aae52e813822ca9
Anon <lexdss@gmail.com>
2017-03-28 00:30:55 +0300

    Add UploadFile behavior

---
+++ components/UploadFileBehavior.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\components;
+
+use Yii;
+use yii\base\Behavior;
+use yii\web\UploadedFile;
+use yii\db\ActiveRecord;
+use app\models\helpers\FileHelper;
+
+class UploadFileBehavior extends Behavior
+{
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_AFTER_VALIDATE => 'saveFile'
+        ];
+    }
+
+    public function saveFile($event)
+    {
+        if ($file = UploadedFile::getInstance($event->sender, 'file')){
+            $fileName = FileHelper::getRandomPath($file);
+
+            if ($file->saveAs(Yii::getAlias('@uploadroot') . $fileName)) {
+                $event->sender->img = Yii::getAlias('@uploadweb') . $fileName;
+            } else {
+                var_dump('dsfsdf');
+                exit;
+            }
+        }
+    }
+}
